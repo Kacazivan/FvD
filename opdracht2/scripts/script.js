fdd160f4c97b7f9f4269c0b6bf05bf8cb33a0627
Kacazivan <112778173+Kacazivan@users.noreply.github.com>
2023-06-17 15:20:42 +0200

    Achtergrond per lied toegevoegd

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -2,12 +2,9 @@
 console.log("howdy");
 
 /* getting started --> https://swiperjs.com/get-started */
-// Stap 1. --> klik op het tandwiel (van de CSS en JS)
-// Stap 2. --> Kijk in de HTML
-// Stap 3.
 var options = {
   direction: 'horizontal', //richting van de carousel - de default
-  loop: 'true', // van 25 naar 1 en vice versa
+  // loop: 'true', // van 25 naar 1 en vice versa
   speed: 300, // duur van transitie in ms
   cssMode: true, // smoother
 
@@ -26,11 +23,3 @@ var options = {
 
 /* het daadwerkelijk initialiseren van de carousel */
 const swiper = new Swiper('.swiper', options);
-
-
-
-// Stap 4. --> Kijk in de CSS (bij :root en onderaan)
-
-
-
-/* hopla klaar */
