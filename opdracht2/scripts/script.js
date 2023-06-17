48b687c0902da6658407893f715dde707ab4bab9
Kacazivan <112778173+Kacazivan@users.noreply.github.com>
2023-06-17 22:02:10 +0200

    geprobeerd audio toe te voegen

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -2,19 +2,15 @@
 console.log("howdy");
 
 /* getting started --> https://swiperjs.com/get-started */
+
 var options = {
   direction: 'horizontal', //richting van de carousel - de default
-  // loop: 'true', // van 25 naar 1 en vice versa
   speed: 300, // duur van transitie in ms
-  cssMode: true, // smoother
-
-  // pagination
+  cssMode: true,
   pagination: {
     el: '.swiper-pagination', // class van de paginering
     type: 'fraction' // x/xx als paginering
   },
-
-  // navigation arrows
   navigation: {
     nextEl: '.swiper-button-next', // class van next button
     prevEl: '.swiper-button-prev' // class van prev button
@@ -23,3 +19,20 @@ var options = {
 
 /* het daadwerkelijk initialiseren van de carousel */
 const swiper = new Swiper('.swiper', options);
+let currentAudio = null;
+
+swiper.on('slideChange', function () {
+  const activeSlide = this.slides[this.activeIndex];
+  const audioElement = activeSlide.querySelector('audio');
+  
+  if (currentAudio) {
+    currentAudio.pause();
+  }
+  
+  if (audioElement) {
+    audioElement.play();
+    currentAudio = audioElement;
+  }
+});
+
+/* EERSTE AUDIO SPEELT NIET AUTOMATISCH AF?? */
